f383313840040cbc3a88c458292de4ac687d1bc1
guangzhi <xugzhi1987@gmail.com>
2020-04-01 18:27:35 +0800

    change sphinx master to index

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ author = 'Guangzhi XU'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
+master_docs = 'index'
 
 # -- General configuration ---------------------------------------------------
 
