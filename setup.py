109a4bd14db3485465d215a876f9a9580059e2da
guangzhi <xugzhi1987@gmail.com>
2020-06-09 03:41:55 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# Image-Processing based Atmospheric River Tracking (IPART) algorithms
+
+from distutils.core import setup
+
+setup(name='ipart',
+        version='2.0',
+        description='IPART is a Python package for the detection and tracking of atmospheric rivers from gridded IVT data using image-processing techniques.',
+        author='Guangzhi XU',
+        author_email='xugzhi1987@gmail.com',
+        url='https://github.com/ihesp/IPART',
+        packages=['ipart',],
+        license='GPL-3'
+        )
+
+
+
